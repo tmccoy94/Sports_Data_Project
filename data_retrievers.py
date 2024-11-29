f02629435e3300427c4b06d942b2561fda645a5a
Taylor McCoy <mccoyt57@gmail.com>
2024-11-29 15:13:47 -0600

    successful google sheets test

---
+++ data_retrievers.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import gspread
+from gspread_dataframe import set_with_dataframe
+from dbmanager import SqliteDBManager
+
+
+class GsheetsRetriever:
+    def __init__(self):
+        pass
